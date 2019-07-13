b83b8436afb875ec87774084c749eb4077171e12
Beenish-sheikh <50364249+beenish-sheikh@users.noreply.github.com>
2019-07-13 12:41:51 -0700

    Add files via upload
    using different data types like Boolean, array,JSON,numbers ,strings,

---
+++ script.js
@@ -0,0 +1,197 @@
+let techKaro = {
+
+    ceo: "sadaf abid",
+    owner: "head of circle ",
+    sponsers: [
+        "engro pakistan",
+        "circle",
+        "techKaro"
+    ],
+
+    missions: [" Led by Sadaffe Abid, CIRCLE Pakistan's mission is to advance women's economic ",
+        "participation and empowerment through innovative entrepreneurship",
+        "CIRCLE introduces She Loves Tech Pakistan Global Startup Competition at NED University ",
+    ],
+    course: "web development",
+    courseOutlines: [
+        "HTML",
+        "CSS",
+        "JAVASCRIPT"
+    ],
+
+    duration: "8 months",
+
+    teachers: [
+        "ma'am sarah ",
+        "miss arsala",
+        "miss noreen",
+        "sir Ali"
+    ],
+
+    studentsName: [{
+
+            name: "beenish sheikh",
+            Qulification: "masters in international Relations",
+            cnic: "12345679",
+            phone: "09764321",
+            address: "12 st no 10 block A gulistan e jouher,karachi",
+            Email: "abc123@gmail.com",
+            isEmployed: true,
+
+        },
+
+        {
+
+            name: "beenish sheikh",
+            Qulification: "masters in international Relations",
+            cnic: "12345679",
+            phone: "09764321",
+            address: "12 st no 10 block A gulistan e jouher,karachi",
+            Email: "abc123@gmail.com",
+            isEmployed: true,
+
+        },
+
+        {
+
+            name: "nida sheikh",
+            Qulification: "masters in education",
+            cnic: 12345679,
+            phone: "09764321",
+            address: "12 st no 10 block A gulistan e jouher,karachi",
+            Email: "abc123@gmail.com",
+            isEmployed: false,
+
+        },
+
+        {
+
+            name: "sameera",
+            Qulification: "masters in public  Relations",
+            cnic: 12345679,
+            phone: "09764321",
+            address: "12jouher,karachi",
+            Email: "abc123@gmail.com",
+            isEmployed: true,
+
+        },
+
+
+
+        {
+
+            name: "khadija",
+            Qulification: "mba",
+            cnic: 12345679,
+            phone: "09764321",
+            address: "gulistan e jouher,karachi",
+            Email: "abc123@gmail.com",
+            isEmployed: true,
+
+        },
+
+        {
+
+            name: "areeba ",
+            Qulification: "f.sc",
+            cnic: 12345679,
+            phone: "09764321",
+            address: "orangi town,karachi",
+            Email: "abc123@gmail.com",
+            isEmployed: false,
+
+        },
+
+        {
+
+            name: "armia afridee",
+            Qulification: "b.sc hons",
+            cnic: 12345679,
+            phone: "09764321",
+            address: "12 st no 10 block A gulistan e jouher,karachi",
+            Email: "abc123@gmail.com",
+            isEmployed: true,
+
+        },
+
+        {
+
+            name: "jamila fatima",
+            Qulification: "b.a",
+            cnic: 12345679,
+            phone: "09764321",
+            address: "sachul,karachi",
+            Email: "abc123@gmail.com",
+            isEmployed: false,
+
+        },
+
+        {
+
+            name: "syda kokub",
+            Qulification: "ssc",
+            cnic: 12345679,
+            phone: "09764321",
+            address: "rabia city,karachi",
+            Email: "abc123@gmail.com",
+            isEmployed: false,
+
+        },
+
+        {
+
+            name: " sheikh uzair",
+            Qulification: "masters in media ",
+            cnic: 12345679,
+            phone: "09764321",
+            address: "gulshan,karachi",
+            Email: "abc123@gmail.com",
+            isEmployed: true,
+
+        },
+
+        {
+
+            name: "reena bano",
+            Qulification: "international Relations",
+            cnic: 12345679,
+            phone: "09764321",
+            address: "block A gulistan e jouher,karachi",
+            Email: "abc123@gmail.com",
+            isEmployed: true,
+
+        },
+
+        {
+
+            name: "aysha scationheikh",
+            Qulification: "masters in edu",
+            cnic: 12345679,
+            phone: "09764321",
+            address: "12 st no 10 block A,karachi",
+            Email: "abc123@gmail.com",
+            isEmployed: false,
+
+        },
+
+
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
